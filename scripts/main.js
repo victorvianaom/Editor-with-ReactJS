3d6b8e55af9595692dbfb8dec3adf32adfd162a9
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-06-19 20:44:31 -0300

    converted buttons and their logic to React

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,33 @@
 class MathButton extends React.Component {
-    render() {}
+    constructor(props) {
+        super(props);
+        this.clickHandler = this.clickHandler.bind(this);
+    }
+    clickHandler() {
+        editorMathField.cmd(this.props.com);
+    }
+    render() {
+        return React.createElement(
+            'button',
+            { onClick: this.clickHandler },
+            this.props.val
+        );
+    }
 }
+ReactDOM.render(React.createElement(
+    'div',
+    null,
+    React.createElement(MathButton, { com: '\\sqrt', val: 'RAIZ' }),
+    React.createElement(MathButton, { com: '\\frac', val: 'FRACAO' }),
+    React.createElement(MathButton, { com: '\\pm', val: '+ -' }),
+    React.createElement(MathButton, { com: '^', val: React.createElement(
+            'span',
+            null,
+            'x',
+            React.createElement(
+                'sup',
+                null,
+                'y'
+            )
+        ) })
+), document.getElementById('buttons'));
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,23 @@
 class MathButton extends React.Component {
+    constructor(props) {
+        super(props)
+        this.clickHandler = this.clickHandler.bind(this)
+    }
+    clickHandler() {
+        editorMathField.cmd(this.props.com)
+    }
     render() {
-        
+        return (
+                <button onClick={this.clickHandler}>{this.props.val}</button>
+        )
     }
 }
+ReactDOM.render(
+    <div>
+        <MathButton com={'\\sqrt'} val='RAIZ'/>
+        <MathButton com={'\\frac'} val='FRACAO'/>
+        <MathButton com={'\\pm'} val='+ -'/>
+        <MathButton com={'^'} val={<span>x<sup>y</sup></span>}/>
+    </div>,
+    document.getElementById('buttons')
+)
